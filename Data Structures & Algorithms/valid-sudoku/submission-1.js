c567d96b75a90b8b488bdb5b3c7725ad7b8d9480
Mani-Vardhan-Kasthuri <84430559+Mani-Vardhan-Kasthuri@users.noreply.github.com>
2026-04-18 12:28:36 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.js
@@ -0,0 +1,55 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @return {boolean}
+     */
+    isValidSudoku(board) {
+        // rows
+        for(let i=0; i<9; i++) {
+            const seen = Array(10).fill(0);
+            for(let j=0; j<9; j++) {
+                const val = board[i][j];
+                if(val !== ".") {
+                    if(seen[+val]) return false;
+                    seen[+val] = 1;
+                }
+            }
+        }
+
+        // cols
+        for(let i=0; i<9; i++) {
+            const seen = Array(10).fill(0);
+            for(let j=0; j<9; j++) {
+                const val = board[j][i];
+                if(val !== ".") {
+                    if(seen[+val]) return false;
+                    seen[+val] = 1;
+                }
+            }
+        }
+
+        // 3*3 grid
+        let col=0, row=0;
+        while(col<9 && row<9) {
+            let rc = col + 2;
+            let br = row + 2;
+            const seen = Array(10).fill(0);
+            for(let r = row; r <= br; r++) {
+                for(let c=col; c<=rc; c++) {
+                    const val = board[r][c];
+                    if(val !== ".") {
+                        if(seen[+val]) return false;
+                        seen[+val] = 1;
+                    }
+                }
+            }
+            col = rc + 1;
+            if(col === 9) {
+                col = 0;
+                row = row + 3;
+            }
+        }
+
+        return true;
+    }
+}
